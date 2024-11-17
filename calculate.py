8538e7933d0fae978962234f320294406c9a8097
tamimiqbal42 <tamimiqbal1710547899@gmail.com>
2024-11-18 00:19:41 +0600

    math oparation added

---
+++ calculate.py
@@ -0,0 +1,21 @@
+import math
+
+# Floor and Ceiling
+print(math.floor(4.8))
+print(math.ceil(4.8))
+
+# Constants
+print(math.pi)
+print(math.e)
+
+# Greatest Common Division
+num1 = 22
+num2= 32
+print(math.gcd(num1,num2))
+
+# Least common Multiple
+print(math.lcm(num1,num2))
+
+# Opertor Precedence
+oparaterPower=10+3*2
+print(oparaterPower)
